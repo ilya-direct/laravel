f4112e343cb7eec1235c5231539ee32e297af875
Ilya Smirnov <ilya-direct@ya.ru>
2017-06-20 00:15:44 +0300

    Request validation and dependency resolver

--- app/Http/Routes/front.php
+++ app/Http/Routes/front.php
@@ -3,3 +3,6 @@
 
 Route::get('/', 'SiteController@index');
 Route::get('/page/{id}', 'SiteController@showPage');
+Route::get('/love', function(\App\Http\Requests\TestBlog $request) {
+    return 'xxx';
+});
